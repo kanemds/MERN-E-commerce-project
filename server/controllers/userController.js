d151d156dec80ec3db5683da3ffa0b000eb5b50b
Kanem <kanemdeng@email.com>
2022-11-22 01:36:26 -0800

    setting up CRUD for user route

---
+++ server/controllers/userController.js
@@ -0,0 +1,22 @@
+const User = require('../models/User')
+const Note = require('../models/Note')
+const asynceHandler = require('express-async-handler')
+const bcrypt = require('bcrypt')
+
+const getAllUsers = asynceHandler(async (req, res) => {
+
+})
+
+const createUser = asynceHandler(async (req, res) => {
+
+})
+
+const updateUser = asynceHandler(async (req, res) => {
+
+})
+
+const deleteUser = asynceHandler(async (req, res) => {
+
+})
+
+module.exports = { getAllUsers, createUser, updateUser, deleteUser }
