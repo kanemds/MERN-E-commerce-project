884e157567937dec6b338143b0bfbed1e80ad0b6
Kanem <kanemdeng@email.com>
2022-11-22 00:48:42 -0800

    setting up routes CRUD for user

--- server/routes/root.js
+++ server/routes/root.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const user = require('./api/user')
 
 router.get('^/$|/index(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, '../', 'views', 'index.html'))
 })
+
+router.use('/user', user)
 
 module.exports = router
