240731c21112735ca22b6c595480e3fd94efedf4
Kanem <kanemdeng@email.com>
2022-11-23 10:32:06 -0800

    added created route for Note model

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -10,7 +10,31 @@ const getAllNotes = asynceHandler(async (req, res) => {
   res.json(allNotes)
 })
 
-const createNote = asynceHandler(async (req, res) => { })
+const createNote = asynceHandler(async (req, res) => {
+  const { user, title, text } = req.body
+
+  if (!user || !title || !text) {
+    return res.status(400).json({ message: 'All fields are required' })
+  }
+
+  const duplicate = await Note.findOne({ title }).lean().exec()
+
+  if (duplicate) {
+    return res.status(409).json({ message: 'Title already exist, please try again' })
+  }
+
+  const noteObject = { user, title, text }
+
+  const note = await Note.create(noteObject)
+
+  if (note) {
+    res.status(201).json({ message: `note: ${title} created` })
+  } else {
+    res.status(400).json({ message: 'Invalid note data received' })
+  }
+})
+
+
 const updateNote = asynceHandler(async (req, res) => { })
 const deleteNote = asynceHandler(async (req, res) => { })
 
