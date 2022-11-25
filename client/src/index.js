75e79422735f678f90af0e9a538b6d81579d08a6
Kanem <kanemdeng@email.com>
2022-11-25 10:48:14 -0800

    adding provider and store from redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import CssBaseline from '@mui/material/CssBaseline'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <Provider store={store}>
+      <CssBaseline />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
