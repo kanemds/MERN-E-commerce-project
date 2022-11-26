c1dd90cef27dc13e8234bf9c79d76466b2206ccd
Kanem <kanemdeng@email.com>
2022-11-26 12:57:11 -0800

    adding table for display users

--- client/src/pages/users/UsersList.js
+++ client/src/pages/users/UsersList.js
@@ -1,6 +1,27 @@
 import React from 'react'
 import { useGetUsersQuery } from './usersApiSlice'
-import { Typography } from '@mui/material'
+import { Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material'
+
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein }
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+]
+
+
 
 const UserList = () => {
 
@@ -11,7 +32,8 @@ const UserList = () => {
     isError,
     error
   } = useGetUsersQuery()
-  console.log(error)
+
+
 
   let content
 
@@ -25,8 +47,49 @@ const UserList = () => {
     }</Typography>
   }
 
+  if (isSuccess) {
+    const { ids } = users
+    console.log(users)
+    console.log(ids)
+
+    // const tableContent = ids?.length ?
+    //   ids.map(userId => <User key={userId} userId={userId} />)
+    //   : null
+
+
+
+  }
+
   return (
-    <div>UserList</div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
 
