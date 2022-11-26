a9d8bede6e8d082de37b5f3a748883a5e1f18f49
Kanem <kanemdeng@email.com>
2022-11-26 13:00:56 -0800

    creating User.js for usersList table content

---
+++ client/src/pages/users/User.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { selectUserById } from './usersApiSlice'
+
+const User = () => {
+  return (
+    <div>User</div>
+  )
+}
+
+export default User
