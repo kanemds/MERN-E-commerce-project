d222518976c90254d56320ff7da177376fa06f41
Kanem <kanemdeng@email.com>
2022-11-26 12:00:57 -0800

    creating uersList

--- client/src/pages/users/UsersList.js
+++ client/src/pages/users/UsersList.js
@@ -1,6 +1,30 @@
 import React from 'react'
+import { useGetUsersQuery } from './usersApiSlice'
+import { Typography } from '@mui/material'
 
 const UserList = () => {
+
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error
+  } = useGetUsersQuery()
+  console.log(error)
+
+  let content
+
+  if (isLoading) {
+    content = <Typography>Loading...</Typography>
+  }
+
+  if (isError) {
+    content = <Typography>{
+      error?.data?.message
+    }</Typography>
+  }
+
   return (
     <div>UserList</div>
   )
