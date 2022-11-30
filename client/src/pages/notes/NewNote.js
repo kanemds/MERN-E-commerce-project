a45fd2b75ac4d3f284317ec1716922c0d48599a7
Kanem <kanemdeng@email.com>
2022-11-29 22:20:03 -0800

    custom a job postion selection form

--- client/src/pages/notes/NewNote.js
+++ client/src/pages/notes/NewNote.js
@@ -5,14 +5,6 @@ import { ROLES } from '../../config/roles'
 
 const NewNote = () => {
 
-  // included
-  const USER_REGEX = /^[a-zA-Z0-9-_.]{3,24}$/
-  // required type
-  const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
-
-  const NewUserForm = () => {
-    const [addNewUser,] = useAddNewUserMutation()
-  }
 
   return (
     <div>NewNote</div>
