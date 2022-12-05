31d5ecf60cf6b90584204c47c3bf6752476899b0
Kanem <kanemdeng@email.com>
2022-12-04 22:52:27 -0800

    added logout logic with clear cookie method

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -84,7 +84,10 @@ const refresh = (req, res) => {
 }
 
 const logout = (req, res) => {
-
+  const cookies = req.cookies
+  if (!cookies?.jwt) return res.sendStatus(204)
+  res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true })
+  res.json({ message: 'Cookie Cleared' })
 }
 
 module.exports = { login, refresh, logout }
