c11a72dd409d4d57d467a9379289698725149c83
Kanem <kanemdeng@email.com>
2022-12-19 13:20:11 -0800

    able to create new customer and query

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -1,9 +1,50 @@
-const customer = require('../models/customer')
+const Customer = require('../models/customer')
 const bcrypt = require('bcrypt')
 require('express-async-errors')
 
-const getAllCustomers = async (req, res) => { }
-const createCustomer = async (req, res) => { }
+const getAllCustomers = async (req, res) => {
+  const customers = await Customer.find().select('password').lean()
+  if (!customers.length) {
+    return res.status(400).json({ message: 'No Customer Found' })
+  }
+  res.json(customers)
+}
+
+const createCustomer = async (req, res) => {
+  const { customername, email, password } = req.body
+  console.log(customername, email, password)
+
+  if (!customername || !email || !password) {
+    return res.status(400).json({ message: 'All fields are required' })
+  }
+
+  const duplicateName = await Customer.findOne({ customername }).collation({ locale: 'en', strength: 2 }).lean().exec()
+
+  if (duplicateName) {
+    return res.status(409).json({ message: 'Customer Name already exist, Please try another one' })
+  }
+
+  const duplicateEmail = await Customer.findOne({ email }).collation({ locale: 'en', strength: 2 }).lean().exec()
+
+  if (duplicateEmail) {
+    return res.status(409).json({ message: 'Email already exist, Please try another one' })
+  }
+
+  const hashed = await bcrypt.hash(password, 10)
+
+  const info = { customername, email, password: hashed }
+
+  const newCustomer = Customer.create(info)
+
+  if (newCustomer) {
+    res.status(201).json({ message: `New Customer: '${customername}' has been created` })
+  } else {
+    res.status(400).json({ message: 'Invalid customer data received' })
+  }
+
+}
+
+
 const updateCustomer = async (req, res) => { }
 const deleteCustomer = async (req, res) => { }
 
--- server/models/customer.js
+++ server/models/customer.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const customerSchema = new mongoose.Schema({
-  username: {
+  customername: {
     type: String,
     required: true
   },
