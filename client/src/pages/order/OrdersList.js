830ae43920d911fb6e1fc44a139f3850bbe6e6c9
Kanem <kanemdeng@email.com>
2023-01-17 13:27:31 -0800

    added order breakpoint

--- client/src/pages/order/OrdersList.js
+++ client/src/pages/order/OrdersList.js
@@ -3,6 +3,7 @@ import React from 'react'
 import LoadingMessage from '../../components/LoadingMessage'
 import useAuth from '../../hooks/useAuth'
 import { useGetOrdersQuery } from './ordersApiSlice'
+import Grid from '@mui/material/Unstable_Grid2'
 
 import Order from './Order'
 
@@ -36,19 +37,33 @@ const OrdersList = () => {
 
     content = (
       <TableContainer component={Paper} sx={{ height: '100%' }}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <Table aria-label="simple table">
           <TableHead>
-            <TableRow>
-              <TableCell>Order Id</TableCell>
-              <TableCell align="left">Purchased At</TableCell>
-              <TableCell align="left">Customer Name</TableCell>
-              <TableCell align="left">Customer Email</TableCell>
-              <TableCell align="center">Edit</TableCell>
+            <TableRow sx={{ borderBottom: '1px solid grey', '&:last-child td, &:last-child th': { border: 0 } }}>
+              <Grid container sx={{ display: 'flex', alignItems: 'center' }} >
+                <Grid xs={8} sm={6} md={4} >
+                  <TableCell>Order Id</TableCell>
+                </Grid>
+                <Grid xs={0} sm={3} md={2} sx={{ display: 'flex' }}>
+                  <TableCell sx={{ display: { xs: 'none', sm: 'inline', md: 'inline' } }}>Purchased At</TableCell>
+                </Grid>
+                <Grid xs={0} sm={0} md={2} >
+                  <TableCell sx={{ display: { xs: 'none', sm: 'none', md: 'inline' } }}>Name</TableCell>
+                </Grid>
+                <Grid xs={0} sm={0} md={2} >
+                  <TableCell sx={{ display: { xs: 'none', sm: 'none', md: 'inline' } }}>Email</TableCell>
+                </Grid>
+                <Grid xs={4} sm={1} md={1} >
+                  <TableCell >Edit</TableCell>
+                </Grid>
+              </Grid>
+
             </TableRow>
+
           </TableHead>
           {tableContent}
         </Table>
-      </TableContainer>
+      </TableContainer >
     )
   }
 
